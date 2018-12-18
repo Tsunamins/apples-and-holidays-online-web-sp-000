5f950822dd22f45176908f4231705c60f679c1f2
Alison Reilly <reillyamr@gmail.com>
2018-12-18 01:00:25 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -97,7 +97,7 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.each do |season, next_level|
     next_level.each do |holiday, item|
       if item.include?("BBQ") == true 
-        new_array << item
+        new_array << holiday
       
     
       end 
